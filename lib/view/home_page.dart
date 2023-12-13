bf09e8eb5510bc169f87228e72468de4fef8b1e2
bang400 <bangbang.freelife720@gmail.com>
2023-12-14 00:05:21 +0900

    add: 画面わけ

---
+++ lib/view/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(child: Text("try chat app"))
+    );
+  }
+}
