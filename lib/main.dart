18e7635e68d72daac14222968c109e03e1f35d6d
bang400 <bangbang.freelife720@gmail.com>
2024-03-30 13:55:34 +0900

    add: firebase初期化関連

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'pages/chat/view/chat_page.dart';
 import 'pages/home_page.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   const app = MyApp();
   const scope = ProviderScope(child: app);
+
+  // Firebase初期化処理
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(scope);
 }
 
@@ -19,8 +29,10 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('tryChatApp'),
-        ),
+        ),        
+        // 初回インストール時、ログアウト時はホームページへ
         body: const HomePage(),
+        // ログインしていたらチャットページへ
         // body: const ChatPage(),
       ),
     );
